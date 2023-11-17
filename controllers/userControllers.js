f754b1652618b2291f4234adc1896bf6f6d93995
agungsyon <ahmatagung645@gmail.com>
2023-11-18 00:47:35 +0700

    feat: add endpoint edit profile user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -6,36 +6,20 @@ const { User } = require("../models");
 class userControllers {
   static async register(req, res, next) {
     try {
+      console.log(req.file);
       const { username, email, password, role, phoneNumber, address } = req.body;
 
-      await imageKit.upload(
-        {
-          file: req.file.buffer.toString("base64"),
-          fileName: `${Date.now()}_${req.file.originalname}`,
-          folder: "BB_User",
-          useUniqueFileName: false,
-        },
-        async function (err, fileResponse) {
-          if (err) {
-            return res.status(500).json({
-              message: "Error occured during photo upload. Please try again.",
-            });
-          }
+      const user = await User.create({
+        username,
+        email,
+        password,
+        role,
+        phoneNumber,
+        address,
+      });
+      const access_token = signToken({ id: user.id, email: user.email });
 
-          const user = await User.create({
-            username,
-            email,
-            password,
-            role,
-            phoneNumber,
-            address,
-            profilePicture: fileResponse.url,
-          });
-          const access_token = signToken({ id: user.id, email: user.email });
-
-          res.status(201).json({ access_token, role: user.role });
-        }
-      );
+      res.status(201).json({ access_token, role: user.role });
     } catch (error) {
       next(error);
     }
@@ -63,6 +47,36 @@ class userControllers {
       res.status(200).json({ access_token, username: user.username, role: user.role });
     } catch (error) {
       console.log(error);
+      next(error);
+    }
+  }
+
+  static async editProfilePicture(req, res, next) {
+    try {
+      await imageKit.upload(
+        {
+          file: req.file.buffer.toString("base64"),
+          fileName: `${Date.now()}_${req.file.originalname}`,
+          folder: "BB_User",
+          useUniqueFileName: false,
+        },
+        async function (err, fileResponse) {
+          if (err) {
+            return res.status(500).json({
+              message: "Error occured during photo upload. Please try again.",
+            });
+          }
+          const id = req.user.id;
+          const user = await User.findByPk(id);
+          if (!user) {
+            throw { status: 404, message: "User Not Found" };
+          }
+          await user.update({ profilePicture: fileResponse.url });
+
+          res.status(200).json({ message: "Success Edit Profile Picture" });
+        }
+      );
+    } catch (error) {
       next(error);
     }
   }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const userControllers = require("../controllers/userControllers");
 const upload = require("../middlewares/multer");
+const authentication = require("../middlewares/authentication");
 const router = express.Router();
 
-router.post("/register", upload.single('profilePicture'), userControllers.register);
+router.post("/register", userControllers.register);
 router.post("/login", userControllers.login);
+router.use(authentication);
+router.put("/users", upload.single("profilePicture"), userControllers.editProfilePicture);
 
 module.exports = router;
