21c0714cc34fb4ae1c90179569b99901e1dcc114
Riovaldy Wijaya <riovaldy1@gmail.com>
2023-11-18 11:34:42 +0700

    fix: port and debug

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
-const express = require("express");
-const router = require("./routes");
-const cors = require("cors");
-const errorHandling = require("./middlewares/errorHandling");
+const express = require('express');
+const router = require('./routes');
+const cors = require('cors');
+const errorHandling = require('./middlewares/errorHandling');
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Test Server BeliBang");
+app.get('/', (req, res) => {
+  res.send('Test Server BeliBang');
 });
 
 app.use(router);
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,7 @@
-const { comparePassword } = require("../helpers/bcrypt");
-const imageKit = require("../helpers/imageKit");
-const { signToken } = require("../helpers/jwt");
-const { User } = require("../models");
+const { comparePassword } = require('../helpers/bcrypt');
+const imageKit = require('../helpers/imageKit');
+const { signToken } = require('../helpers/jwt');
+const { User } = require('../models');
 
 class userControllers {
   static async register(req, res, next) {
@@ -29,10 +29,10 @@ class userControllers {
     try {
       const { email, password } = req.body;
       if (!email) {
-        throw { status: 400, message: "Email is required" };
+        throw { status: 400, message: 'Email is required' };
       }
       if (!password) {
-        throw { status: 400, message: "Password is required" };
+        throw { status: 400, message: 'Password is required' };
       }
 
       const user = await User.findOne({
@@ -40,7 +40,7 @@ class userControllers {
       });
 
       if (!user || !comparePassword(password, user.password)) {
-        throw { status: 401, message: "Invalid email/password" };
+        throw { status: 401, message: 'Invalid email/password' };
       }
 
       const access_token = signToken({ id: user.id, email: user.email });
@@ -55,25 +55,25 @@ class userControllers {
     try {
       await imageKit.upload(
         {
-          file: req.file.buffer.toString("base64"),
+          file: req.file.buffer.toString('base64'),
           fileName: `${Date.now()}_${req.file.originalname}`,
-          folder: "BB_User",
+          folder: 'BB_User',
           useUniqueFileName: false,
         },
         async function (err, fileResponse) {
           if (err) {
             return res.status(500).json({
-              message: "Error occured during photo upload. Please try again.",
+              message: 'Error occured during photo upload. Please try again.',
             });
           }
           const id = req.user.id;
           const user = await User.findByPk(id);
           if (!user) {
-            throw { status: 404, message: "User Not Found" };
+            throw { status: 404, message: 'User Not Found' };
           }
           await user.update({ profilePicture: fileResponse.url });
 
-          res.status(200).json({ message: "Success Edit Profile Picture" });
+          res.status(200).json({ message: 'Success Edit Profile Picture' });
         }
       );
     } catch (error) {
