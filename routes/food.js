68754bd55ccd6d91fbd18ecdff6e17e1329510e0
agungsyon <ahmatagung645@gmail.com>
2023-11-20 11:46:32 +0700

    fix: conflict routes

--- routes/food.js
+++ routes/food.js
@@ -4,16 +4,9 @@ const foodControllers = require("../controllers/foodControllers")
 const { sellerAuthorization, foodOwnerAuthorization } = require("../middlewares/authorization")
 const upload = require("../middlewares/multer")
 
-<<<<<<< HEAD
 router.post("/foods", sellerAuthorization, upload.single('imageUrl'), foodControllers.createFood);
 router.get("/foods/:id", foodControllers.findFood);
 router.put("/foods/:id", foodOwnerAuthorization, upload.single('imageUrl'), foodControllers.updateFood);
 router.delete("/foods/:id", foodOwnerAuthorization, foodControllers.deleteFood);
-=======
-router.post("/foods", sellerAuthorization, upload.single('imageUrl'), foodControllers.createFood)
-router.get("/foods/:id", foodControllers.findFood)
-router.put("/foods/:id", foodOwnerAuthorization, foodControllers.updateFood)
-router.delete("/foods/:id", foodOwnerAuthorization, foodControllers.deleteFood)
->>>>>>> origin/testing
 
 module.exports = router
